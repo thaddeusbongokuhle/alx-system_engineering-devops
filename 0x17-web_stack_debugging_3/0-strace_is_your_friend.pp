c2b453c69eb27b27072af68076e44afaff18dddf
Bongokuhle <thaddeusbongokuhle@gmail.com>
2024-06-19 23:04:37 -0700

    web stack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fixi type in wp-settings.php file
+exec { 'fix-wp-error-log':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
